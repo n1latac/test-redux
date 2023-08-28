02d4835971bb2b4fcca3772c8bd6b16c8950575d
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-08-28 22:31:49 +0300

    add confirm password line(front)

--- client/src/validation/index.js
+++ client/src/validation/index.js
@@ -1,8 +1,9 @@
-import {string, object} from 'yup'
+import {string, object, ref} from 'yup'
 
 export const registrationSchema = object({
     firstName: string().required('first name is required'),
     lastName: string().required('last name is required'),
-    email: string().required('email is required'),
-    password: string().required('password is required'),
+    email: string().required('email is required').email('must be a valid email'),
+    password: string().required('password is required').matches("(?=.*?[A-Z])",'At least one upper case English letter'),
+    confirmPassword: string().required('Please retype your password').oneOf([ref('password')], 'Your password dont match')
 })

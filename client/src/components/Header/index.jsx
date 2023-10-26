1e4853b0f2b4d9c86838872997ec201999573cd9
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-10-27 01:53:11 +0300

    fix navBar closing

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import PopUpExit from '../PopUpExit'
@@ -6,12 +6,24 @@ import PopUpExit from '../PopUpExit'
 
 function Header(props) {
   const [navMenu, setNavMenu] = useState(false)
-  
-  useEffect(()=>{
-    
-  },[])
+  const navBarRef = useRef(null)
 
-  const handleSideBar = (()=>setNavMenu(navMenu=>!navMenu))
+  const closeNavMenu = () => {
+    if(navBarRef.current){
+      setNavMenu(navMenu=>!navMenu)
+    }else{
+      setNavMenu(false)
+    }
+  }
+
+  useEffect(()=>{
+    document.addEventListener('click', closeNavMenu)
+
+    return ()=>{
+      document.removeEventListener('click', closeNavMenu)
+    }
+  },[navMenu])
+
 
   return (
     <header class="bg-bg bg-cover bg-opacity-1 sticky top-0 z-10">
@@ -33,7 +45,11 @@ function Header(props) {
         </nav>
         {props.user
           ?
-          <PopUpExit/>
+          <PopUpExit>
+            <button class="py-2 px-3 border-2 border-gray-500 bg-slate-300 hover:transform hover:scale-110 transition-all duration-200" >
+            Выйти
+          </button>
+            </PopUpExit>
           :
         <Link to={'/signUp'}>
           <button class="py-2 px-3 border-2 border-transparen border-gray-500 bg-slate-300 hover:transform hover:scale-110 transition-all duration-200">
@@ -47,28 +63,33 @@ function Header(props) {
           <img src='./images/logo_1.ico' class="w-8 h-8 hover:cursor-pointer" alt='logo' />
         </Link>
         {navMenu ?
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" color='white' cursor='pointer' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8  hover:scale-110" onClick={handleSideBar}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" color='white' cursor='pointer' viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8  hover:scale-110">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
 
           :
-          <svg cursor='pointer' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 transition-all duration-[2000ms] hover:scale-110" onClick={handleSideBar}>
+          <svg ref={navBarRef} cursor='pointer' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 transition-all duration-[2000ms] hover:scale-110">
             <path strokeLinecap="round" strokeLinejoin="round" color='white' d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
           </svg>
         }
         {navMenu ?
         props.user ? 
-        <div className='fixed top-16 right-0 active:top-16'>
-            <ul className='bg-bg'>
-            <li className='p-2 text-center'><Link to={'/'} class="header-link">Главная</Link></li>
-            <li className='p-2 text-center'><Link to={'/mainPage'} class="header-link">Посты</Link></li>
-            <li className='p-2 text-center'><Link to={'ownPosts'} class="header-link">Мои статьи</Link></li>
-            <li className='p-2 text-center'><Link to={'/addPost'} class="header-link">Добавить статью</Link></li>
-            {/* <li className='p-2 text-center'><Link to={'/signUp'} class="header-link">Выйти</Link></li> */}
-            </ul>
-          </div>
-        :
-          <div className='fixed top-16 right-0 active:top-16'>
+            <div className='fixed top-16 right-0 active:top-16'>
+              <ul className='bg-bg'>
+                <li className='p-2 text-center'><Link to={'/'} class="header-link">Главная</Link></li>
+                <li className='p-2 text-center'><Link to={'/mainPage'} class="header-link">Посты</Link></li>
+                <li className='p-2 text-center'><Link to={'ownPosts'} class="header-link">Мои статьи</Link></li>
+                <li className='p-2 text-center'><Link to={'/addPost'} class="header-link">Добавить статью</Link></li>
+                <li className='p-2 text-center'><PopUpExit>
+                  <span class="header-link cursor-pointer">
+                    Выйти
+                  </span>
+                </PopUpExit>
+                </li>
+              </ul>
+            </div>
+            :
+            <div className='fixed top-16 right-0 active:top-16'>
             <ul className='bg-bg'>
             <li className='p-2 text-center'><Link to={'/'} class="header-link">Главная</Link></li>
             <li className='p-2 text-center'><Link to={'/mainPage'} class="header-link">Посты</Link></li>
--- client/src/components/PopUpExit/index.jsx
+++ client/src/components/PopUpExit/index.jsx
@@ -6,9 +6,7 @@ import {userExitRequest} from '../../actions/actionCreator'
 
 function PopUpExit(props) {
   return (
-    <Popup trigger={<button class="py-2 px-3 border-2 border-gray-500 bg-slate-300 hover:transform hover:scale-110 transition-all duration-200" >
-            Выйти
-          </button>} modal lockScroll overlayStyle={{"backgroundColor": "rgba(0, 0, 0, 0.5)"}} nested>
+    <Popup trigger={props.children} modal lockScroll overlayStyle={{"backgroundColor": "rgba(0, 0, 0, 0.5)"}} nested>
             {close=>(
                 <div class="flex flex-col justify-between items-center w-[500px] h-[250px] py-10 border-2 bg-indigo-300 rounded-xl">
                 <p>Do you want exit from your account?</p>

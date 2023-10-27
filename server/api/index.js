511a96a7fa9c0b40b78a0ae5c64ca639215752bb
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-10-27 06:25:27 +0300

    ch

---
+++ server/api/index.js
@@ -0,0 +1,19 @@
+const {Router} = require('express')
+const userRouter = require('./userRoutes')
+const postRouter = require('./postRouter')
+const commentRouter = require('./commentRouter')
+const router = Router()
+
+router.use('/user', userRouter)
+router.use('/post', postRouter)
+router.use('/comment', commentRouter)
+router.use('/test',async(req, res)=>{
+    try {
+        res.send('hello3')
+    } catch (error) {
+        
+    }
+})
+
+
+module.exports = router

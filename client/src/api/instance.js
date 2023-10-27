32ee3127d793a36cdf473cf41b8ffa12b64bad18
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-10-27 06:33:02 +0300

    change

--- client/src/api/instance.js
+++ client/src/api/instance.js
@@ -5,7 +5,7 @@ import {refreshSessionRequest} from '../actions/actionCreator'
  
 
 export const instance = axios.create({
-    baseURL: 'https://test-redux-server-k8ezv5l83-n1latac.vercel.app/api'
+    baseURL: 'https://test-redux-server-phi.vercel.app/api'
 })
 
 instance.interceptors.request.use(function(config){

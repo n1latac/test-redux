4073650182af06390f60b62a8f89e39e8635a287
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2023-10-27 07:13:12 +0300

    again change secret field

--- server/services/tokenAuth.js
+++ server/services/tokenAuth.js
@@ -4,23 +4,26 @@ const jwt = require('jsonwebtoken')
 const promisifyJwtSign = promisify(jwt.sign)
 const promisifyJwtVerify = promisify(jwt.verify)
 
+const accessSecret = 'vcyeveyrebcDSJ'
+const refreshSecret = 'AFKSDFHSKDFJkdjh23sfkslk234'
+
 
 module.exports.createToken = async({userId, email})=>{
-    return await promisifyJwtSign({userId, email}, 'vcyeveyrebcDSJ',{
+    return await promisifyJwtSign({userId, email}, accessSecret,{
         expiresIn: 60*60
     })
 }
 
 module.exports.verifyToken = async(token)=>{
-    return await promisifyJwtVerify(token, 'vcyeveyrebcDSJ')
+    return await promisifyJwtVerify(token, accessSecret)
 }
 
 module.exports.createRefreshToken = async({userId, email}) => {
-    return await promisifyJwtSign({userId, email}, 'AFKSDFHSKDFJkdjh23sfkslk234',{
+    return await promisifyJwtSign({userId, email}, refreshSecret,{
         expiresIn: '7d'
     })
 }
 
 module.exports.verifyRefreshToken = async(token)=>{
-    return await promisifyJwtVerify(token, 'AFKSDFHSKDFJkdjh23sfkslk234')
+    return await promisifyJwtVerify(token, refreshSecret)
 }
